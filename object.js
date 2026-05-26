ead7fffa2f58f3dd6314eb0cd222e218b7661997
joy0789 <johirulislamj62@gmail.comm>
2026-05-26 12:47:47 +0600

    Introduction to Objects properties and values

---
+++ object.js
@@ -0,0 +1,18 @@
+// object
+const bottle = {
+    brand: 'Pure',
+    price: 25,
+    color: 'white',
+    isClean: true
+}
+
+const subject = {
+    name = 'biology',
+    teacher = 'rasheda mam',
+    examDate = '30 Feb',
+    chapters = ['first', 'second', 'third'],
+    exams: {
+        name: 'Final Exam',
+        marks: 100
+    }
+}
