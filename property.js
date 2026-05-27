0689456bd7c944e6708775cdeac96d1304c2b408
joy0789 <johirulislamj62@gmail.comm>
2026-05-27 12:20:24 +0600

    Multiple ways to get & set object properties

---
+++ property.js
@@ -0,0 +1,14 @@
+const person = {
+    name: 'Joy',
+    age: 25,
+    profession: 'developer',
+    salary: 25000,
+    married: false
+}
+// dot notation
+console.log(person.profession);
+const income = person.salary;
+console.log(income);
+
+// bracket notation
+console.log(person['age']);
---
+++ value.js
@@ -0,0 +1,10 @@
+const person = {
+    name: 'Joy',
+    age: 25,
+    profession: 'developer',
+    salary: 25000,
+    married: false
+}
+person.salary = 30000;
+person['age'] = 26;
+console.log(person);
